44c1ffc9b25bb35b016f7f83ef03acef2a3821a0
Pushkar Sinha <sinhapushkar047@gmail.com>
2026-09-18 02:09:44 +0530

    Delete llm.py directory

--- llm.py/llm_openai.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import OpenAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-model=OpenAI(model="gpt-5.6-luna")
-
-result=model.invoke("Write a 5 line on AI")
-print(result)
